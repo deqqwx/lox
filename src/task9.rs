534b2a6f84dec2c70b97c9aea023ea102b787479
deqqwx <d.yevtyshenko@e-u.edu.ua>
2024-10-04 12:40:29 +0300

    task9.rs

---
+++ src/task9.rs
@@ -0,0 +1,11 @@
+#[test]
+fn main() {
+    let (x, y); // Declare variables x and y
+    (x, ..) = (3, 4); // Destructure to assign x
+    [.., y] = [1, 2]; // Destructure to assign y
+
+    // Fill the blank to make the code work
+    assert_eq!([x, y], [3, 2]); // Fill in with [3, 2]
+
+    println!("Success!");
+}
