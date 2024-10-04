9cb44aeae3af68bd3bdf0817279118ce78a9771e
deqqwx <d.yevtyshenko@e-u.edu.ua>
2024-10-04 13:36:37 +0300

    task9.rs

---
+++ src/Variables/mod.rs
@@ -0,0 +1,9 @@
+mod task1;
+mod task2;
+mod task3;
+mod task4;
+mod task5;
+mod task6;
+mod task7;
+mod task8;
+mod task9;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
+
+mod variables;
 mod task1;
-mod task2;
-mod task3;
-mod task4;
-mod task5;
-mod task6;
-mod task7;
-mod task8;
-mod task9;
 
 fn main() {
     println!("Hello, world!");
