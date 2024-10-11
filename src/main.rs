248e00ff0c1e61ee31c1e1ed1142fa290870109e
deqqwx <d.yevtyshenko@e-u.edu.ua>
2024-10-11 12:37:18 +0300

    numbers10.rs

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod numbers6;
 mod numbers7;
 mod numbers8;
 mod numbers9;
+mod numbers10;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/numbers10.rs
@@ -0,0 +1,8 @@
+#[test]
+use std::ops::{Range, RangeInclusive};
+fn main() {
+    assert_eq!((1..5), Range{ start: 1, end: 5 });          // Діапазон від 1 до 5 (5 не включений)
+    assert_eq!((1..=5), RangeInclusive::new(1, 5));         // Діапазон від 1 до 5 (5 включений)
+
+    println!("Success!");
+}
