10a84868d62c2200c1a7c1abab7bcd89f0b7d400
deqqwx <d.yevtyshenko@e-u.edu.ua>
2024-10-18 13:06:40 +0300

    char1

---
+++ src/basictypes/char1.rs
@@ -0,0 +1,12 @@
+#[test]
+fn main() {
+    use std::mem::size_of_val;
+
+    let c1 = 'a';
+    assert_eq!(size_of_val(&c1), 4);
+
+    let c2 = '中';
+    assert_eq!(size_of_val(&c2), 4);
+
+    println!("Success!");
+}
